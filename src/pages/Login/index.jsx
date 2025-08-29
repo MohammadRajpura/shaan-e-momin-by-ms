c37f6b8a4c9f127fd09d67332401774757520f55
MohammadRajpura <rajpuramohammad123@gmail.com>
2025-08-29 12:59:01 +0530

    Update dynamic part on login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,55 @@
+import { useEffect, useState } from "react";
 import Button from "../../components/Button/Index";
+import { api } from "../../@core/APIs/api";
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  country_code: z.string(),
+  mobile_number: z.number({ error: "Phone number is required" }),
+});
 
 const Login = () => {
+  const [countryCodes, setCountryCodes] = useState([]);
+
+  // Connection to Zod
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  // ** Call Country code API
+  useEffect(() => {
+    api
+      .get("country-code-list")
+      .then((res) => {
+        console.log(res.data);
+
+        if (res.data.status == "success") {
+          setCountryCodes(res.data.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const onSubmit = (data) => {
+    console.log(data);
+
+    // Loader -
+    api
+      .post("login-with-otp", data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="auth-wrapper">
       {/* Banner */}
@@ -11,16 +60,32 @@ const Login = () => {
       {/* Auth Part */}
       <div className="auth-part-wrap">
         {/* Our Form */}
-        <form className="form-group">
+        <form className="form-group" onSubmit={handleSubmit(onSubmit)}>
           <div className="form-control">
             <label>Mobile Number</label>
             <div className="input-with-select">
-              <select>
-                <option value="+91">+91</option>
-                <option value="+92">+92</option>
-              </select>
-              <input type="text" />
+              {countryCodes?.length > 0 && (
+                <select {...register("country_code")}>
+                  {countryCodes.map((item, index) => {
+                    return (
+                      // eslint-disable-next-line react/jsx-key
+                      <option key={index} value={item.country_code}>
+                        {item.country_code}
+                      </option>
+                    );
+                  })}
+                </select>
+              )}
+
+              <input
+                type="number"
+                {...register("mobile_number", { valueAsNumber: true })}
+              />
             </div>
+
+            {errors?.mobile_number?.message && (
+              <span className="text-red">{errors?.mobile_number?.message}</span>
+            )}
           </div>
 
           <Button
